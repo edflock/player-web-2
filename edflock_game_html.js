a4a8cbdc5f8fd2814276522f09e82bb942272ff7
ujjwal Shukla <ujjwal.shukla.mec09@itbhu.ac.in>
2015-09-18 23:35:32 +0530

    changed

--- edflock_game_html.js
+++ edflock_game_html.js
@@ -1,156 +1,216 @@
 (function(window, $, PubSub) {
-	var edFlockGameWebTransport = function (argument) {
+    var edFlockGameWebTransport = function (argument) {
 
-		var argument = argument || {},
-			iframe = argument.iframe || window;
+        var argument = argument || {},
+            iframe = argument.iframe || window;
 
-		if (window.addEventListener) {
-			// For standards-compliant web browsers
-			iframe.addEventListener("message", parseMessage, false);
-		}
-		else {
-			iframe.attachEvent("onmessage", parseMessage);
-		}
+        if (window.addEventListener) {
+            // For standards-compliant web browsers
+            iframe.addEventListener("message", parseMessage, false);
+        }
+        else {
+            iframe.attachEvent("onmessage", parseMessage);
+        }
 
-		function parseMessage(event) {
-			var messageData = event.data,
-				channel = messageData.channel,
-				topic = messageData.topic,
-				actualData = messageData.data;
-			messageName = channel + "." + topic;
-			console.log('main message just recieved = ' + messageName);
-			PubSub.publish(messageName, actualData);
-		}
+        function parseMessage(event) {
+            var messageData = event.data,
+                channel = messageData.channel,
+                topic = messageData.topic,
+                actualData = messageData.data;
+            messageName = channel + "." + topic;
+            console.log('main message just recieved = ' + messageName);
+            PubSub.publish(messageName, actualData);
+        }
 
-		this.publish = function(options) {
-			var channel = options.channel || 'generic',
-				topic = options.topic || '',
-				data = options.data || "";
+        this.publish = function(options) {
+            var channel = options.channel || 'generic',
+                topic = options.topic || '',
+                data = options.data || "";
 
-			var dataMain = {
-				channel: channel,
-				topic: topic,
-				data: data
-			};
+            var dataMain = {
+                channel: channel,
+                topic: topic,
+                data: data
+            };
 
-			messageName = channel + "." + topic;
-			
-			console.log('main publishing event = '+ messageName);
-			PubSub.publish(messageName, data);
+            messageName = channel + "." + topic;
 
-			iframe.postMessage(dataMain, "*");
-		}
+            console.log('main publishing event = '+ messageName);
+            PubSub.publish(messageName, data);
 
-		this.subscribe = function(options) {
-			var channel = options.channel || 'generic',
-				topic = options.topic || '',
-				callback = options.callback || function() {console.log('no call back given')};
+            iframe.postMessage(dataMain, "*");
+        };
 
-			messageName = channel + "." + topic;
-			console.log('main subscribe to message = ' + messageName);
-			PubSub.subscribe(messageName, callback);
-		}
+        this.subscribe = function(options) {
+            var channel = options.channel || 'generic',
+                topic = options.topic || '',
+                callback = options.callback || function() {console.log('no call back given')};
 
-		this.subscribeOnce = function (options) {
-			var channel = options.channel || 'generic',
-				topic = options.topic || '',
-				callback = options.callback || function() {console.log('no call back given')};
+            messageName = channel + "." + topic;
+            console.log('main subscribe to message = ' + messageName);
+            PubSub.subscribe(messageName, callback);
+        };
 
-			messageName = channel + "." + topic;
-			console.log('main subscribing to event once = '+ messageName);
+        this.subscribeOnce = function (options) {
+            var channel = options.channel || 'generic',
+                topic = options.topic || '',
+                callback = options.callback || function() {console.log('no call back given')};
 
-			subscribeId = PubSub.subscribe(messageName, callback);
-			PubSub.subscribe(messageName, (function(token) {
-				return function() {
-					PubSub.unsubscribe( token );
-				}
-			})(subscribeId));
-		}
-	}
-	window.EdflockGameWeb = function(iframeElementId) {
-		// TODO: See to fix this
-		/*var users = edFlockGameWebTransport.channel('users'),
-			score = edFlockGameWebTransport.channel('score');*/
+            messageName = channel + "." + topic;
+            console.log('main subscribing to event once = '+ messageName);
 
-		/*users.subscribe('username', function(data, envelope) {
-		    var result = "gotcha";
-		    console.log('recieved')
-		    envelope.reply(null, result);
-		});*/
+            subscribeId = PubSub.subscribe(messageName, callback);
+            PubSub.subscribe(messageName, (function(token) {
+                return function() {
+                    PubSub.unsubscribe( token );
+                }
+            })(subscribeId));
+        }
+    };
+    window.EdflockGameWeb = function(iframeElementId) {
+        // TODO: See to fix this
+        /*var users = edFlockGameWebTransport.channel('users'),
+         score = edFlockGameWebTransport.channel('score');*/
 
-		element = document.getElementById(iframeElementId).contentWindow;
+        /*users.subscribe('username', function(data, envelope) {
+         var result = "gotcha";
+         console.log('recieved')
+         envelope.reply(null, result);
+         });*/
 
-		var transport = new edFlockGameWebTransport({iframe: element});
-		
-		this.transport = transport;
-		/* It expect options as 
-		*	{
-		*		channel : "",
-		*		topic : '',
-		*		dataFunction: function() {}
-		*	}
-		*
-		* Subscribes to topic = 'topic.request' and publish the data in the form of 'topic.response'
-		*/
-		this.subscribeAndReply = function(options) {
-			var channel = options.channel || 'generic',
-				topic = options.topic || '#',
-				dataFunction = options.dataFunction || function() {return{}};
+        element = document.getElementById(iframeElementId).contentWindow;
 
-			this.transport.subscribe({
-				channel  : channel,
-				topic    : topic + '-request',
-				callback : function() {
-					var data = dataFunction();
-        		    transport.publish({
-	                    channel: channel,
-	                    topic: topic + "-response",
-	                    data: data
-	                });
-				}
-			});
-		}
+        var transport = new edFlockGameWebTransport({iframe: element});
 
-		this.requestForData = function(options) {
-			var channel = options.channel || 'generic',
-				topic = options.topic || '#',
-				callback = options.callback || function() {console.log('no call back given')};
+        this.transport = transport;
+        /* It expect options as
+         *	{
+         *		channel : "",
+         *		topic : '',
+         *		dataFunction: function() {}
+         *	}
+         *
+         * Subscribes to topic = 'topic.request' and publish the data in the form of 'topic.response'
+         */
+        this.subscribeAndReplyWithFunction = function(options) {
+            var channel = options.channel || 'generic',
+                topic = options.topic || '#',
+                dataFunction = options.dataFunction || function() {return{}};
 
-			this.transport.publish({
+            this.transport.subscribe({
+                channel  : channel,
+                topic    : topic + '-request',
+                callback : function() {
+                    var data = dataFunction();
+                    transport.publish({
+                        channel: channel,
+                        topic: topic + "-response",
+                        data: data
+                    });
+                }
+            });
+        };
+
+        this.subscribeAndReplyWithData = function(options) {
+            var channel = options.channel || 'generic',
+                topic = options.topic || '#',
+                data = options.data || '';
+
+            this.transport.subscribe({
+                channel  : channel,
+                topic    : topic + '-request',
+                callback : function() {
+                    var data = dataFunction();
+                    transport.publish({
+                        channel: channel,
+                        topic: topic + "-response",
+                        data: data
+                    });
+                }
+            });
+        };
+
+        this.subscribeAndReplyAsync = function(options) {
+            var channel = options.channel || 'generic',
+                topic = options.topic || '#',
+                promise = options.promise;
+
+            this.transport.subscribe({
+                channel  : channel,
+                topic    : topic + '-request',
+                callback : function() {
+                    promise.done(function(data) {
+                        transport.publish({
+                            channel: channel,
+                            topic: topic + "-response",
+                            data: data
+                        });
+                    });
+                }
+            });
+        };
+
+        this.requestForData = function(options) {
+            var channel = options.channel || 'generic',
+                topic = options.topic || '#',
+                callback = options.callback || function() {console.log('no call back given')};
+
+            this.transport.publish({
                 channel: channel,
                 topic: topic + "-request"
             });
 
-			this.transport.subscribe({
-				channel  : channel,
-				topic    : topic + "-response",
-				callback : callback
-			}).once();
-		}
+            this.transport.subscribe({
+                channel  : channel,
+                topic    : topic + "-response",
+                callback : callback
+            }).once();
+        };
 
-		this.provideUserName = function(dataFunction) {
-			this.subscribeAndReply({channel: 'users', topic: 'name', dataFunction: dataFunction});
-		}
+        this.provideUser = function(promise) {
+            this.subscribeAndReplyAsync({channel: 'users', topic: 'all', promise: promise});
+        };
 
-		this.subscribePointAdded = function(cb) {
-			this.transport.subscribe({
-				channel: 'points',
-				topic: 'points-add',
-				callback: cb
-			});
-		}
+        this.subscribePointAdded = function(cb) {
+            this.transport.subscribe({
+                channel: 'points',
+                topic: 'points-add',
+                callback: cb
+            });
+        };
 
-		this.subscribeStatusChange = function(cb) {
-			this.transport.subscribe({
-				channel: 'status',
-				topic: 'status-change',
-				callback: cb
-			});
-		}
+        this.subscribeStatusChange = function(cb) {
+            this.transport.subscribe({
+                channel: 'status',
+                topic: 'status-change',
+                callback: cb
+            });
+        };
 
-	}
-	
-}) (window, $, PubSub)
+        this.subscribeLifeUpdate = function (cb) {
+            this.transport.subscribe({
+                channel: 'life',
+                topic: 'update',
+                callback: cb
+            })
+        };
 
+        this.subscribeStatusLost = function (cb) {
+            this.transport.subscribe({
+                channel: 'status',
+                topic: 'lost',
+                callback: cb
+            })
+        };
 
+        this.subscribeBadgeAwarded = function(cb) {
+            this.transport.subscribe({
+                channel: 'status',
+                topic: 'badge-awarded',
+                callback: cb
+            })
+        }
 
+    }
+
+}) (window, $, PubSub);
--- main.js
+++ main.js
@@ -1,20 +1,49 @@
 $( function() {
-	edflockGameWeb = new EdflockGameWeb("iframe");
-	edflockGameWeb.provideUserName((function() {
-		var x = 1;
-		return function() {
-			return "ujjwal" + (x++);
-		}
-	}) ());
+    edflockGameWeb = new EdflockGameWeb("iframe");
+    var point = 0;
 
-	edflockGameWeb.subscribePointAdded(function(d,e) {
-		console.log('recieved');
+    function updatePoints(pt) {
+        point += pt;
+        $('#points').html(point);
+    }
 
-		$('#points').append(e)
-	});
+    function awardBadge(data) {
+        $("#badgeName").html(data.name);
+        $("#badgeImage").attr('src', data.badgeUrl);
+        $("#badgeName").data('id', data.id);
+        $("#badgeModal").modal('show');
+    }
 
-	edflockGameWeb.subscribeStatusChange(function(d,e) {
-		$('#status').html(e)
-	});
-	/*edflockGameWeb.initialize();*/
+    function updateLife(life) {
+        var sel = $("#lifeIndicator i:gt("+(life-1)+")");
+        sel.each(function(index, elem) {
+            $(elem).removeClass('fa-heart').addClass('fa-heart-o');
+        })
+    }
+
+
+    edflockGameWeb.provideUser((function() {
+        var promise = $.get('http://api.edflock.com/user/id/'+userId);
+        return promise;
+    }) ());
+
+    edflockGameWeb.subscribePointAdded(function(d,e) {
+        updatePoints(e);
+    });
+
+    edflockGameWeb.subscribeLifeUpdate(function(d,e) {
+        updateLife(e);
+    });
+
+    edflockGameWeb.subscribeStatusChange(function(d,e) {
+        $('#status').html(e)
+    });
+
+    edflockGameWeb.subscribeBadgeAwarded(function(d,e) {
+        awardBadge(e);
+    });
+    edflockGameWeb.subscribeStatusLost(function(d,e) {
+        alert('lost with points: ' + e.points);
+    });
+    /*edflockGameWeb.initialize();*/
 });
